840f4e8fddd13abaa06d4c89c163af06dcc589f5
MRK <64604054+mrk991000@users.noreply.github.com>
2024-12-23 21:53:45 +0330

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,6 @@ import { TailcastLogo } from "../assets/logos/TailcastLogo";
 import { TwitterIcon } from "../assets/icons/TwitterIcon";
 
 const footerData = [
-  
   {
     title: "Shortcuts",
     items: ["Download", "Features", "Feedback", "FAQ"],
@@ -14,9 +13,10 @@ const footerData = [
 export const Footer = () => {
   return (
     <footer aria-label="Site footer">
-      <div className="pt-10  lg:pt-20 lg:pb-16 bg-bgDark1 radius-for-skewed ">
+      <div className="pt-10 lg:pt-20 lg:pb-16 bg-bgDark1 radius-for-skewed">
         <div className="container mx-auto px-4 w-4/5 md:w-11/12 lg:w-10/12 xl:w-4/5 2xl:w-2/3">
           <div className="flex flex-wrap">
+            {/* Left section with logo */}
             <div className="w-full lg:w-1/3 mb-16 lg:mb-0">
               <div className="flex justify-center lg:justify-start items-center grow basis-0">
                 <div className="text-white mr-2 text-6xl">
@@ -26,43 +26,41 @@ export const Footer = () => {
                   MDM
                 </div>
               </div>
-  
+
               <div className="w-24 mx-auto lg:mx-0">
                 <a
-                  className="inline-block w-10  h-10 mr-2 p-2 pt-[0.55rem] outlined-button"
+                  className="inline-block w-10 h-10 mr-2 p-2 pt-[0.55rem] outlined-button"
                   href="#"
                   aria-label="Facebook"
                 >
-                  <InstagramIcon />
+                  <FacebookIcon />
                 </a>
                 <a
-                  className="inline-block w-10  h-10 mr-2 p-2 pt-[0.55rem] pl-[0.55rem] outlined-button"
+                  className="inline-block w-10 h-10 mr-2 p-2 pt-[0.55rem] pl-[0.55rem] outlined-button"
                   href="#"
                   aria-label="Twitter"
                 >
-                  <InstagramIcon />
+                  <TwitterIcon />
                 </a>
-                
               </div>
             </div>
-           
-            
-              <div className="w-full md:w-1/3 lg:w-auto">
-                <h3 className="mb-6 text-2xl font-bold text-primaryText">Shortcuts</h3>
-                <ul>
-                  {footerData[0].items.map((item, index) => (
-                    <li key={`${item}-${index}`} className="mb-4">
-                      <a
-                        className="text-gray-400 hover:text-gray-300"
-                        href="#home"
-                        aria-label={item}
-                      >
-                        {item}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
+
+            {/* Right section with shortcuts */}
+            <div className="w-full md:w-1/3 lg:w-auto">
+              <h3 className="mb-6 text-2xl font-bold text-primaryText">Shortcuts</h3>
+              <ul>
+                {footerData[0].items.map((item, index) => (
+                  <li key={`${item}-${index}`} className="mb-4">
+                    <a
+                      className="text-gray-400 hover:text-gray-300"
+                      href="#"
+                      aria-label={item}
+                    >
+                      {item}
+                    </a>
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
         </div>
