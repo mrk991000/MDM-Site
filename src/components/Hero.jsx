e16841ba10fa296d1fc2470f5e844102790c8392
MRK <64604054+mrk991000@users.noreply.github.com>
2024-12-23 20:15:03 +0330

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ export const Hero = () => {
           transition={{ duration: 0.5 }}
         >
           <h3 className="text-secondaryColor text-sm sm:text-base  mb-6 sm:mt-32 mt-16  font-bold">
-            Discover new data flow
+            MDM
           </h3>
         </motion.div>
         <motion.div
@@ -28,11 +28,11 @@ export const Hero = () => {
           transition={{ duration: 0.5, delay: 0.05 }}
         >
           <div className="text-5xl sm:text-6xl lg:text-7xl xl:text-7xl font-bold tracking-wide  text-primaryText  px-8 sm:px-8 md:px-20 lg:px-4">
-            <h1 className="inline md:hidden">Analytics</h1>
+            <h1 className="inline md:hidden">Modern</h1>
             <h1 className="hidden md:inline">Analytic tools suite</h1>
           </div>
           <h1 className="mt-2 sm:mt-2 text-4xl sm:text-6xl lg:text-7xl xl:text-7xl font-bold tracking-wide  text-primaryText  px-8 sm:px-20 md:px-24 lg:px-24">
-            for developers
+            Download Manager
           </h1>
         </motion.div>
         <motion.div
@@ -56,14 +56,14 @@ export const Hero = () => {
               onClick={() => setIsModalOpen(true)}
               aria-label="Get started"
             >
-              Get Started
+          Download
             </button>
             <button
               className="w-64 sm:w-52 h-12 rounded-xl font-bold text-primaryText border border-solid  flex justify-center items-center cursor-pointer bg-bgDark2 hover:bg-bgDark3 border-primaryColor transition"
               onClick={() => setIsModalOpen(true)}
               aria-label="Live demo"
             >
-              Live demo
+              Other Versions
             </button>
           </div>
         </motion.div>
