5a837a3e613ec32c1afd198c88e9c150e77ec63d
MRK <64604054+mrk991000@users.noreply.github.com>
2024-12-23 22:25:04 +0330

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -52,14 +52,20 @@ export const Hero = () => {
           <div className="flex flex-col gap-2 sm:flex-row mt-14 mb-24 sm:mb-40 justify-center">
             <button
               className="contained-button w-64 sm:w-52 h-12 mr-0 sm:mr-4 lg:mr-6 mb-2 sm:mb-0"
-              onClick={() => setIsModalOpen(true)}
+              onClick=onClick={(e) => {
+      e.preventDefault();
+      window.location.href='http://google.com';
+      }}
               aria-label="Download"
             >
           Download
             </button>
             <button
               className="w-64 sm:w-52 h-12 rounded-xl font-bold text-primaryText border border-solid  flex justify-center items-center cursor-pointer bg-bgDark2 hover:bg-bgDark3 border-primaryColor transition"
-              onClick={() => setIsModalOpen(true)}
+              onClick=onClick={(e) => {
+      e.preventDefault();
+      window.location.href='http://google.com';
+      }}
               aria-label="Other Versions"
             >
               Other Versions
