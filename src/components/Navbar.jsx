fc9a6c9a7790f0109d7514ca3c2021bc7c146e7a
MRK <64604054+mrk991000@users.noreply.github.com>
2024-12-23 23:20:42 +0330

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,13 +28,6 @@ export const Navbar = () => {
           </a>
         </motion.div>
 
-        {/* Links Container (Right-aligned) */}
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.3 }}
-          exit={{ opacity: 0 }}
-        >
         
 
       {/* Mobile Navbar */}
