d8d4e384f73cb80e3389824fc3d1f8efdcd45e93
MRK <64604054+mrk991000@users.noreply.github.com>
2024-12-23 22:19:28 +0330

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,15 +21,15 @@ export const Footer = () => {
               <div className="w-24 mx-auto lg:mx-0">
                 <a
                   className="inline-block w-10 h-10 mr-2 p-2 pt-[0.55rem] outlined-button"
-                  href="#"
-                  aria-label="Facebook"
+                  href="https://discord.gg/cyXwEgY3"
+                  aria-label="Discord"
                 >
                   <InstagramIcon  />
                 </a>
                 <a
                   className="inline-block w-10 h-10 mr-2 p-2 pt-[0.55rem] pl-[0.55rem] outlined-button"
-                  href="#"
-                  aria-label="Twitter"
+                  href="https://telegram.me/moderndownloadmanager"
+                  aria-label="Telegram"
                 >
                   <InstagramIcon  />
                 </a>
